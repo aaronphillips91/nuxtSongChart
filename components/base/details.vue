b00d782986f30c7a1d5261bda4a46cf2b433ad3e
Aaron Phillips <aaronphillips91@icloud.com>
2024-06-30 20:29:26 -0500

    Update details.vue

--- components/base/details.vue
+++ components/base/details.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>{{ song }}</div>
+  <BaseCard class="flex-col">
+    <div>BASIC INFO</div>
+    <div class="flex gap-2">
+      <img
+        :src="song.art"
+        class="rounded-lg size-48" />
+      <div class="flex flex-col gap-2">
+        <div>Song Title: {{ song.title }}</div>
+        <div>Album: {{ song.album }}</div>
+        <div>Artist: {{ song.artist }}</div>
+      </div>
+    </div>
+    <UDivider class="my-4" />
+    <div>MUSICAL INFO</div>
+    <div class="flex gap-4">
+      <div>Song Tempo: {{ song.tempo }} BPM</div>
+      <div>Original Key: {{ song.original_key }}</div>
+    </div>
+  </BaseCard>
 </template>
 
 <script setup>
